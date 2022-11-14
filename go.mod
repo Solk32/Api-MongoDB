689025fc5eb787338359a534a8c0305baf80334d
Solk32 <nesolk13@gmail.com>
2022-11-15 00:04:03 +0300

    not work MongoDB

--- go.mod
+++ go.mod
@@ -1,3 +1,35 @@
 module awesomeProject2
 
 go 1.19
+
+require (
+	github.com/go-martini/martini v0.0.0-20170121215854-22fa46961aab
+	github.com/martini-contrib/render v0.0.0-20150707142108-ec18f8345a11
+	github.com/russross/blackfriday v1.6.0
+	labix.org/v2/mgo v0.0.0-20140701140051-000000000287
+)
+
+require (
+	github.com/codegangsta/inject v0.0.0-20150114235600-33e0aa1cb7c0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/kr/pretty v0.2.1 // indirect
+	github.com/kr/text v0.1.0 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/testify v1.6.1 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.1 // indirect
+	github.com/xdg-go/stringprep v1.0.3 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.mongodb.org/mongo-driver v1.11.0 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	launchpad.net/gocheck v0.0.0-20140225173054-000000000087 // indirect
+)
--- main.go
+++ main.go
@@ -1,91 +1,165 @@
 package main
 
 import (
+	"awesomeProject2/db/documents"
 	"awesomeProject2/models"
+	"awesomeProject2/session"
+	"awesomeProject2/utils"
 	"fmt"
+	"github.com/martini-contrib/render"
 	"html/template"
 	"net/http"
+	"time"
+
+	"github.com/go-martini/martini"
+	"labix.org/v2/mgo"
 )
 
-var posts map[string]*models.Post
+const (
+	COOKIE_NAME = "sessionId"
+)
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
-	if err != nil {
-		fmt.Fprintf(w, err.Error())
-	}
+var postsCollection *mgo.Collection
+var inMemorySession *session.Session
 
-	fmt.Println(posts)
-
-	t.ExecuteTemplate(w, "index", posts)
+func getLoginHandler(rnd render.Render) {
+	rnd.HTML(200, "login", nil)
 }
 
-func writeHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("templates/write.html", "templates/header.html", "templates/footer.html")
-	if err != nil {
-		fmt.Fprintf(w, err.Error())
+func postLoginHandler(rnd render.Render, r *http.Request, w http.ResponseWriter) {
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	fmt.Println(username)
+	fmt.Println(password)
+
+	sessionId := inMemorySession.Init(username)
+
+	cookie := &http.Cookie{
+		Name:    COOKIE_NAME,
+		Value:   sessionId,
+		Expires: time.Now().Add(5 * time.Minute),
 	}
 
-	t.ExecuteTemplate(w, "write", nil)
+	http.SetCookie(w, cookie)
+
+	rnd.Redirect("/")
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("templates/write.html", "templates/header.html", "templates/footer.html")
-	if err != nil {
-		fmt.Fprintf(w, err.Error())
+func indexHandler(rnd render.Render, r *http.Request) {
+	cookie, _ := r.Cookie(COOKIE_NAME)
+	if cookie != nil {
+		fmt.Println(inMemorySession.Get(cookie.Value))
 	}
 
-	id := r.FormValue("id")
-	post, found := posts[id]
-	if !found {
-		http.NotFound(w, r)
+	postDocuments := []documents.PostDocument{}
+	postsCollection.Find(nil).All(&postDocuments)
+
+	posts := []models.Post{}
+	for _, doc := range postDocuments {
+		post := models.Post{doc.Id, doc.Title, doc.ContentHtml, doc.ContentMarkdown}
+		posts = append(posts, post)
 	}
 
-	t.ExecuteTemplate(w, "write", post)
+	rnd.HTML(200, "index", posts)
 }
 
-func savePostHandler(w http.ResponseWriter, r *http.Request) {
+func writeHandler(rnd render.Render) {
+	post := models.Post{}
+	rnd.HTML(200, "write", post)
+}
+
+func editHandler(rnd render.Render, r *http.Request, params martini.Params) {
+	id := params["id"]
+
+	postDocument := documents.PostDocument{}
+	err := postsCollection.FindId(id).One(&postDocument)
+	if err != nil {
+		rnd.Redirect("/")
+		return
+	}
+	post := models.Post{postDocument.Id, postDocument.Title, postDocument.ContentHtml, postDocument.ContentMarkdown}
+
+	rnd.HTML(200, "write", post)
+}
+
+func savePostHandler(rnd render.Render, r *http.Request) {
 	id := r.FormValue("id")
 	title := r.FormValue("title")
-	content := r.FormValue("content")
+	contentMarkdown := r.FormValue("content")
+	contentHtml := utils.ConvertMarkdownToHtml(contentMarkdown)
 
-	var post *models.Post
+	postDocument := documents.PostDocument{id, title, contentHtml, contentMarkdown}
 	if id != "" {
-		post = posts[id]
-		post.Title = title
-		post.Content = content
+		postsCollection.UpdateId(id, postDocument)
 	} else {
-		id = GenerateId()
-		post := models.NewPost(id, title, content)
-		posts[post.Id] = post
+		id = utils.GenerateId()
+		postDocument.Id = id
+		postsCollection.Insert(postDocument)
 	}
 
-	http.Redirect(w, r, "/", 302)
+	rnd.Redirect("/")
 }
 
-func deleteHandler(w http.ResponseWriter, r *http.Request) {
-	id := r.FormValue("id")
+func deleteHandler(rnd render.Render, r *http.Request, params martini.Params) {
+	id := params["id"]
 	if id == "" {
-		http.NotFound(w, r)
+		rnd.Redirect("/")
+		return
 	}
 
-	delete(posts, id)
+	postsCollection.RemoveId(id)
 
-	http.Redirect(w, r, "/", 302)
+	rnd.Redirect("/")
+}
+
+func getHtmlHandler(rnd render.Render, r *http.Request) {
+	md := r.FormValue("md")
+	html := utils.ConvertMarkdownToHtml(md)
+
+	rnd.JSON(200, map[string]interface{}{"html": html})
+}
+
+func unescape(x string) interface{} {
+	return template.HTML(x)
 }
 
 func main() {
 	fmt.Println("Listening on port :3000")
 
-	posts = make(map[string]*models.Post, 0)
+	inMemorySession = session.NewSession()
 
-	// /css/app.css
-	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/write", writeHandler)
-	http.HandleFunc("/edit", editHandler)
-	http.HandleFunc("/delete", deleteHandler)
-	http.HandleFunc("/SavePost", savePostHandler)
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
 
-	http.ListenAndServe(":3000", nil)
+	postsCollection = session.DB("blog").C("posts")
+
+	m := martini.Classic()
+
+	unescapeFuncMap := template.FuncMap{"unescape": unescape}
+
+	m.Use(render.Renderer(render.Options{
+		Directory:  "templates",                         // Specify what path to load the templates from.
+		Layout:     "layout",                            // Specify a layout template. Layouts can call {{ yield }} to render the current template.
+		Extensions: []string{".tmpl", ".html"},          // Specify extensions to load for templates.
+		Funcs:      []template.FuncMap{unescapeFuncMap}, // Specify helper function maps for templates to access.
+		Charset:    "UTF-8",                             // Sets encoding for json and html content-types. Default is "UTF-8".
+		IndentJSON: true,                                // Output human readable JSON
+	}))
+
+	staticOptions := martini.StaticOptions{Prefix: "assets"}
+	m.Use(martini.Static("assets", staticOptions))
+
+	m.Get("/", indexHandler)
+	m.Get("/login", getLoginHandler)
+	m.Post("/login", postLoginHandler)
+	m.Get("/write", writeHandler)
+	m.Get("/edit/:id", editHandler)
+	m.Get("/delete/:id", deleteHandler)
+	m.Post("/SavePost", savePostHandler)
+	m.Post("/gethtml", getHtmlHandler)
+
+	m.Run()
 }
